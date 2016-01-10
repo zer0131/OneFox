a5b353943384a00ce9910ffb8394bda55d41f2c0
zer0131 <zer0131@vip.qq.com>
2016-01-10 19:40:55 +0800

    添加Curl封装类

---
+++ OneFox/Curl.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * @author ryan<zer0131@vip.qq.com>
+ * @desc curl请求封装
+ */
+
+namespace OneFox;
+
+class Curl {
+    private $_ch;
+
+    public function __construct() {
+        //code
+    }
+
+    public function get() {
+        //code
+    }
+
+    public function post() {
+        //code
+    }
+
+    public function put() {
+        //code
+    }
+
+    public function delete() {
+        //code
+    }
+
+    public function request() {
+        //code
+    }
+
+    public function getInfo() {
+        //code
+    }
+
+    public function getError() {
+        //code
+    }
+
+    public function isError() {
+        //code
+    }
+
+    public function close() {
+        //code
+    }
+
+    public function __destruct() {
+        //code
+    }
+}
