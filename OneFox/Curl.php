3c1599c080cd5e66ae07d9b560ae196bf64e3095
zer0131 <zer0131@vip.qq.com>
2016-03-04 23:35:22 +0800

    修改curl封装类，文件上传兼容php版本

--- OneFox/Curl.php
+++ OneFox/Curl.php
@@ -52,7 +52,11 @@ class Curl {
         if ($params) {
             if ($multi && is_array($multi)) {
                 foreach ($multi as $key => $file) {
-                    $params[$key] = curl_file_create($file);//php5.5以后使用这种方式创建file
+                    if (version_compare(PHP_VERSION,'5.5.0','<')) {
+                        $params[$key] = '@'.$file;
+                    } else {
+                        $params[$key] = curl_file_create($file);//php5.5以后使用这种方式创建file
+                    }
                 }
             } else {
                 $this->_config[CURLOPT_POSTFIELDS] = http_build_query($params);
