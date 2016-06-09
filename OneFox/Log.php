ec01a6574e7cb08468cb4a058a0d0549d83c13b9
zer0131 <zer0131@vip.qq.com>
2016-06-09 16:24:44 +0800

    添加常量

--- OneFox/Log.php
+++ OneFox/Log.php
@@ -9,6 +9,15 @@ namespace OneFox;
 
 final class Log {
 
+    const EMERGENCY = 'emergency';
+    const ALERT = 'alert';
+    const CRITICAL = 'critical';
+    const ERROR = 'error';
+    const WARNING = 'warning';
+    const NOTICE = 'notice';
+    const INFO = 'info';
+    const DEBUG = 'debug';
+
     private static $_instance = null;
     //默认配置
     private $_config = array(
