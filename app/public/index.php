e6f87fd0968dca8f83426b35890a3d40eac02766
五环小胖子 <zer0131@vip.qq.com>
2016-08-18 02:21:02 +0800

    修改lib path定义

--- app/public/index.php
+++ app/public/index.php
@@ -24,16 +24,16 @@ define('MODULE_MODE', true);
 //--------是否开启调试模式(默认关闭)--------//
 define('DEBUG', true);
 
-//--------日志目录--------//
+//--------日志目录(默认在app/logs目录下)--------//
 define('LOG_PATH', APP_PATH . DS . 'logs');
 
-//--------配置目录--------//
+//--------配置目录(默认在app/config目录下)--------//
 define('CONF_PATH', APP_PATH . DS . 'config');
 
-//--------模板目录--------//
+//--------模板目录(默认在app/tpl目录下)--------//
 define('TPL_PATH', APP_PATH . DS . 'tpl');
 
-//--------扩展库目录(默认在app/Lib目录下)--------//
-define(LIB_PATH, dirname(APP_PATH) . DS . 'class');
+//--------扩展库目录(默认在app/lib目录下)--------//
+define('LIB_PATH', dirname(APP_PATH) . DS . 'class');
 
 require ONEFOX_PATH . DS . 'Onefox.php';
