bc4099537b7c6dc689ba5a1c10c00bbb3ed7f4a9
五环小胖子 <zer0131@vip.qq.com>
2016-09-26 00:48:55 +0800

    modify

--- onefox/C.php
+++ onefox/C.php
@@ -163,125 +163,6 @@ class C {
     }
 
     /**
-     * 兼容gzip解压
-     * @param string $data
-     * @param string $filename
-     * @param string $error
-     * @param int $maxlength
-     * @return boolean
-     */
-    public static function gzdecode($data, &$filename = '', &$error = '', $maxlength = null) {
-        $len = strlen($data);
-        if ($len < 18 || strcmp(substr($data, 0, 2), "\x1f\x8b")) {
-            $error = "Not in GZIP format.";
-            return null;  // Not GZIP format (See RFC 1952)
-        }
-        $method = ord(substr($data, 2, 1));  // Compression method
-        $flags = ord(substr($data, 3, 1));  // Flags
-        if ($flags & 31 != $flags) {
-            $error = "Reserved bits not allowed.";
-            return null;
-        }
-        // NOTE: $mtime may be negative (PHP integer limitations)
-        $mtime = unpack("V", substr($data, 4, 4));
-        $mtime = $mtime[1];
-        $xfl = substr($data, 8, 1);
-        $os = substr($data, 8, 1);
-        $headerlen = 10;
-        $extralen = 0;
-        $extra = "";
-        if ($flags & 4) {
-            // 2-byte length prefixed EXTRA data in header
-            if ($len - $headerlen - 2 < 8) {
-                return false;  // invalid
-            }
-            $extralen = unpack("v", substr($data, 8, 2));
-            $extralen = $extralen[1];
-            if ($len - $headerlen - 2 - $extralen < 8) {
-                return false;  // invalid
-            }
-            $extra = substr($data, 10, $extralen);
-            $headerlen += 2 + $extralen;
-        }
-        $filenamelen = 0;
-        $filename = "";
-        if ($flags & 8) {
-            // C-style string
-            if ($len - $headerlen - 1 < 8) {
-                return false; // invalid
-            }
-            $filenamelen = strpos(substr($data, $headerlen), chr(0));
-            if ($filenamelen === false || $len - $headerlen - $filenamelen - 1 < 8) {
-                return false; // invalid
-            }
-            $filename = substr($data, $headerlen, $filenamelen);
-            $headerlen += $filenamelen + 1;
-        }
-        $commentlen = 0;
-        $comment = "";
-        if ($flags & 16) {
-            // C-style string COMMENT data in header
-            if ($len - $headerlen - 1 < 8) {
-                return false;    // invalid
-            }
-            $commentlen = strpos(substr($data, $headerlen), chr(0));
-            if ($commentlen === false || $len - $headerlen - $commentlen - 1 < 8) {
-                return false;    // Invalid header format
-            }
-            $comment = substr($data, $headerlen, $commentlen);
-            $headerlen += $commentlen + 1;
-        }
-        $headercrc = "";
-        if ($flags & 2) {
-            // 2-bytes (lowest order) of CRC32 on header present
-            if ($len - $headerlen - 2 < 8) {
-                return false;    // invalid
-            }
-            $calccrc = crc32(substr($data, 0, $headerlen)) & 0xffff;
-            $headercrc = unpack("v", substr($data, $headerlen, 2));
-            $headercrc = $headercrc[1];
-            if ($headercrc != $calccrc) {
-                $error = "Header checksum failed.";
-                return false;    // Bad header CRC
-            }
-            $headerlen += 2;
-        }
-        // GZIP FOOTER
-        $datacrc = unpack("V", substr($data, -8, 4));
-        $datacrc = sprintf('%u', $datacrc[1] & 0xFFFFFFFF);
-        $isize = unpack("V", substr($data, -4));
-        $isize = $isize[1];
-        // decompression:
-        $bodylen = $len - $headerlen - 8;
-        if ($bodylen < 1) {
-            // IMPLEMENTATION BUG!
-            return null;
-        }
-        $body = substr($data, $headerlen, $bodylen);
-        $data = "";
-        if ($bodylen > 0) {
-            switch ($method) {
-                case 8:
-                    // Currently the only supported compression method:
-                    $data = gzinflate($body, $maxlength);
-                    break;
-                default:
-                    $error = "Unknown compression method.";
-                    return false;
-            }
-        }  // zero-byte body content is allowed
-        // Verifiy CRC32
-        $crc = sprintf("%u", crc32($data));
-        $crcOK = $crc == $datacrc;
-        $lenOK = $isize == strlen($data);
-        if (!$lenOK || !$crcOK) {
-            $error = ($lenOK ? '' : 'Length check FAILED. ') . ($crcOK ? '' : 'Checksum FAILED.');
-            return false;
-        }
-        return $data;
-    }
-
-    /**
      * 计算两个时间戳的时间差
      * @param int $begin 开始时间戳
      * @param int $end 结束时间戳
@@ -368,7 +249,7 @@ class C {
      * @param string $encoding 数据编码
      * @return string
      */
-    public static function xml_encode($data, $root='onefox', $item='item', $attr='', $id='id', $encoding='utf-8') {
+    public static function xmlEncode($data, $root='onefox', $item='item', $attr='', $id='id', $encoding='utf-8') {
         if(is_array($attr)){
             $_attr = array();
             foreach ($attr as $key => $value) {
@@ -400,7 +281,7 @@ class C {
                 $key  = $item;
             }
             $xml    .=  "<{$key}{$attr}>";
-            $xml    .=  (is_array($val) || is_object($val)) ? data_to_xml($val, $item, $id) : $val;
+            $xml    .=  (is_array($val) || is_object($val)) ? self::data2Xml($val, $item, $id) : $val;
             $xml    .=  "</{$key}>";
         }
         return $xml;
