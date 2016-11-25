c3a23e777ff3961cef055370d0678f6452205c8a
五环小胖子 <zer0131@vip.qq.com>
2016-11-25 15:05:51 +0800

    修改

--- app/model/index/Test.php
+++ app/model/index/Test.php
@@ -6,7 +6,7 @@ use onefox\Model;
 
 class Test extends Model {
 
-	protected $db_config = 'test';
+	protected $dbConfig = 'test';
 
     public function test(){
         return $this->db->query('select * from `posts`');
