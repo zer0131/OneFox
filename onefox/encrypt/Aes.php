a5c37542edb2bde72476880f4439730e53b2fb82
五环小胖子 <zer0131@vip.qq.com>
2017-10-11 20:10:20 +0800

    添加两个加解密类

---
+++ onefox/encrypt/Aes.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @aythor ryan<zer0131@vip.qq.com>
+ * @desc AES加解密封装
+ */
+
+namespace onefox\encrypt;
+
+class Aes {
+    //code...
+}
---
+++ onefox/encrypt/Rsa.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author ryan<zer0131@vip.qq.com>
+ * @desc RSA加解密封装
+ */
+
+namespace onefox\encrypt;
+
+class Rsa {
+    //code...
+}
